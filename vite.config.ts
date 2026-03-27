6508041e1d56748d890e4c05a18342bf1f539e7b
Serafim Oleynik <serafim.oleynik.5777@yandex.ru>
2026-03-27 15:48:47 +0300

    fix: correct script source path in index.html and comment out VitePWA configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
 import ui from '@nuxt/ui/vite'
-import { VitePWA } from 'vite-plugin-pwa'
+// import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   resolve: {
@@ -22,23 +22,23 @@ export default defineConfig({
         }
       }
     }),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['logo.svg'],
+    // VitePWA({
+    //   registerType: 'autoUpdate',
+    //   includeAssets: ['logo.svg'],
 
-      manifest: {
-        name: 'Biologic-LIMS',
-        short_name: 'biologic-lims',
-        description: 'biologic-lims is a laboratory information management system (LIMS) designed to streamline and optimize laboratory workflows. It provides a comprehensive solution for managing samples, experiments, data, and reporting in a laboratory setting.',
-        theme_color: '#ffffff',
-        icons: [
-          { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/icon-512.png', sizes: '512x512', type: 'image/png' }
-        ]
-      },
-      devOptions: {
-        enabled: false
-      }
-    })
+    //   manifest: {
+    //     name: 'Biologic-LIMS',
+    //     short_name: 'biologic-lims',
+    //     description: 'biologic-lims is a laboratory information management system (LIMS) designed to streamline and optimize laboratory workflows. It provides a comprehensive solution for managing samples, experiments, data, and reporting in a laboratory setting.',
+    //     theme_color: '#ffffff',
+    //     icons: [
+    //       { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
+    //       { src: '/icon-512.png', sizes: '512x512', type: 'image/png' }
+    //     ]
+    //   },
+    //   devOptions: {
+    //     enabled: false
+    //   }
+    // })
   ]
 })
